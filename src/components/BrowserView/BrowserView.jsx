1f025d91f2743690e665526c4b502fb59240fb36
Nicolas Pita <nicolaspita@NMBP.home>
2020-08-15 14:28:17 -0700

    ideate on full browser implementation

--- src/components/BrowserView/BrowserView.jsx
+++ src/components/BrowserView/BrowserView.jsx
@@ -3,10 +3,19 @@ import styles from './BrowserView.module.scss';
 import { GlobalContext } from '../../context/reducers/globalReducer';
 
 const BrowserView = () => {
-  const [{ url }] = useContext(GlobalContext);
+  const [{ url }, dispatchToGlobal] = useContext(GlobalContext);
+
+  // add functionality to set url to whatever the user inputs
+  const changeUrl = dispatchToGlobal();
+
+  // how to do this?
 
   return (
     <>
+      <form action=''>
+        <input type='text' />
+        <input type='submit' />
+      </form>
       <webview id={styles.browserView} src={url} />
     </>
   );
